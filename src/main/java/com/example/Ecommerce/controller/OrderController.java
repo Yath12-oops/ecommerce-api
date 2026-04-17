8e53c11b893c03e83881736db45f397920d12d81
Yatharth <sachdevayatharth12@gmail.com>
2026-04-18 00:34:30 +0530

    implemented placedOrder API

--- src/main/java/com/example/Ecommerce/controller/OrderController.java
+++ src/main/java/com/example/Ecommerce/controller/OrderController.java
@@ -1,15 +1,36 @@
 package com.example.Ecommerce.controller;
 
+import com.example.Ecommerce.dto.request.OrderRequest;
+import com.example.Ecommerce.exceptions.CustomerNotFoundException;
+import com.example.Ecommerce.exceptions.ProductNotFoundException;
+import com.example.Ecommerce.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/v1/order")
 public class OrderController {
 
-//    @PostMapping
-//    public ResponseEntity placeOrder(@RequestParam){
-//
-//    }
+    @Autowired
+    OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity placeOrder(@RequestParam("customer-id") int customerId,
+                                     @RequestBody List<OrderRequest> orderRequests){
+
+        try{
+            String response=orderService.placeOrder(customerId,orderRequests);
+            return new ResponseEntity(response, HttpStatus.CREATED);
+        }
+        catch (CustomerNotFoundException e){
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+        catch (ProductNotFoundException e){
+            return new ResponseEntity(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- src/main/java/com/example/Ecommerce/service/OrderService.java
+++ src/main/java/com/example/Ecommerce/service/OrderService.java
@@ -1,20 +1,66 @@
 package com.example.Ecommerce.service;
 
+import com.example.Ecommerce.dto.request.OrderRequest;
+import com.example.Ecommerce.exceptions.CustomerNotFoundException;
+import com.example.Ecommerce.exceptions.ProductNotFoundException;
 import com.example.Ecommerce.model.Customer;
+import com.example.Ecommerce.model.OrderEntity;
+import com.example.Ecommerce.model.Product;
 import com.example.Ecommerce.repository.CustomerRepo;
+import com.example.Ecommerce.repository.OrderRepo;
+import com.example.Ecommerce.repository.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
 @Service
 public class OrderService {
+
     @Autowired
     CustomerRepo customerRepo;
 
-//    public void placeOrder(int customerId, List<Integer> productIds){
-//
-//        Optional<Customer> customerOptional=CustomerRepo.
-//    }
+    @Autowired
+    ProductRepo productRepo;
+
+    @Autowired
+    OrderRepo orderRepo;
+
+
+    public String placeOrder(int customerId, List<OrderRequest> orderRequests){
+
+        Optional<Customer> customerOptional=customerRepo.findById(customerId);
+        if(customerOptional.isEmpty()){
+            throw  new CustomerNotFoundException("Invalid customer ID: "+ customerId);
+        }
+        Customer customer=customerOptional.get();
+
+        List<Product> products=new ArrayList<>();
+
+            int totalValue=0;
+        for(OrderRequest orderRequest:orderRequests){
+            Optional<Product> productOptional=productRepo.findById(orderRequest.getProductId());
+            if(productOptional.isEmpty()){
+                throw new ProductNotFoundException("Invalid Product Id "+orderRequest.getProductId());
+            }
+            Product product=productOptional.get();
+            if(product.getQuantity()<orderRequest.getQuantity()){
+                throw new ProductNotFoundException("Product out of stock "+orderRequest.getProductId());
+            }
+            products.add(product);
+            totalValue+= product.getPrice()*orderRequest.getQuantity();
+        }
+        OrderEntity order=new OrderEntity();
+        order.setTotalValue(totalValue);
+        order.setCustomer(customer);
+        order.setProducts(products);
+
+        orderRepo.save(order);
+
+        return "Order placed Successfully";
+    }
 }
