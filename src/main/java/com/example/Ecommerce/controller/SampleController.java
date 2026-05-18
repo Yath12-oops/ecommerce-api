a4053ca7ae870dab686e7d10ab3cb13a1602724a
Yatharth <sachdevayatharth12@gmail.com>
2026-05-18 19:04:01 +0530

    add valid endpoint

--- src/main/java/com/example/Ecommerce/controller/SampleController.java
+++ src/main/java/com/example/Ecommerce/controller/SampleController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SampleController {
 
-    @GetMapping
+    @GetMapping("/home")
     public String homepage(){
         return "HOME PAGE";
     }
